ea3ebd31b547491a3310aaac2915b9e47b372c16
Rodion Sibov <rodionsibov@gmail.com>
2022-02-23 14:24:41 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@
 </script>
 
 <template>
- <form action="">
-
- </form>
+  <form data-multi-step>
+    <div data-step="1" class="card">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+      </div>
+      <button type="button">Next</button>
+    </div>
+    <div data-step="2" class="card"></div>
+    <div data-step="3" class="card">
+      <button class="btn-primary" type="submit">Submit</button>
+    </div>
+  </form>
 </template>
 
 <style>
